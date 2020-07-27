4fc779f16ad8ee2cce69ab246e4dfad6da0ad2b6
Carlos Antonio Barros da Silva Júnior <msb_csilva@uolinc.com>
2020-07-27 20:22:13 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ButtonLink(props) {
+    // props => { className: "o que alguém em passar", href: "/" }
+    return (
+        <a href={props.href} className={props.className}>
+            {props.children}
+        </a>
+    )
+}
+
+export default ButtonLink;
