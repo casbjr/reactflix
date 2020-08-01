62f11d7914186f14617a3c7fbed29ba691bbea1a
Carlos Barros <49073667+casbjr@users.noreply.github.com>
2020-08-01 00:48:15 -0300

    hostname

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080'
+  : 'https://reactflixcasbjr.herokuapp.com';
+  
+export default {
+  URL_BACKEND_TOP,
+};
+=======
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
   : 'https://devsoutinhoflix.herokuapp.com';
@@ -5,3 +14,4 @@ const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
 export default {
   URL_BACKEND_TOP,
 };
+>>>>>>> a09416f8acff6a855cc17b883b967f4e5df96654
